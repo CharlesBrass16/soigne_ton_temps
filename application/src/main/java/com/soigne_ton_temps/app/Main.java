d0ff3dde4b5669f7f66aa4ba7787114e8dc0dce2
Charles <charles.brassard.5@ulaval.ca>
2025-01-11 19:17:04 -0500

    test

--- application/src/main/java/com/soigne_ton_temps/app/Main.java
+++ application/src/main/java/com/soigne_ton_temps/app/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Bienvenue dans l'application Soigne Ton Temps!");
 
         // Exemple de logique d'application :
+        // Exemple de logique d'application :
         // Vous pouvez ici ajouter du code pour interagir avec l'utilisateur ou exécuter une logique de base.
 
         System.out.println("Application terminée.");
